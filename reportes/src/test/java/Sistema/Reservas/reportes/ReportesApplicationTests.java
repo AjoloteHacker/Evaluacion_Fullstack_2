43e0b0a6d9eb8183350f29897d45d055db9e1531
bemontoya <be.montoya@duocuc.cl>
2026-04-28 13:25:07 -0400

    Se agregó el model para reportes

---
+++ reportes/src/test/java/Sistema/Reservas/reportes/ReportesApplicationTests.java
@@ -0,0 +1,13 @@
+package Sistema.Reservas.reportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
